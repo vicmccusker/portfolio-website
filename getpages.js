8eaeaa06fa2a46480202521ffa5d2e3528ff39b4
Victoria McCusker <vrmm@hotmail.co.uk>
2024-01-31 16:53:25 +0000

    31/01 v4

---
+++ getpages.js
@@ -0,0 +1,45 @@
+const urlParams = new URLSearchParams(window.location.search); // Gets the data from the URL
+const requestedProject = urlParams.get('project');  
+const content = document.querySelector('.content');
+const header = document.querySelector('.header-project-pages')
+const videoContainer = document.querySelector('.video-container')
+const fullWidth = document.querySelector('.full-width')
+
+
+
+fetch('pages.json').then(function(response) {
+    return response.json()
+})
+
+
+    .then(function (data) {
+
+        data.items.forEach(function (project) {    
+            if (project.title === requestedProject) {
+                console.log("win")  
+                header.innerHTML += 
+                `<h1>${project.name}</h1>`
+
+                fullWidth.innerHTML +=
+                `<img class="main-image-project-page" src="${project.image[1]}"></img>
+                <a id="githubLink" href=${project.github}>View on Github</a>
+                `
+
+                content.innerHTML += 
+                `<div><img class="main-image-project-page" src="${project.image[0]}"></img></div>
+                <p>${project.pageDescription}</p>
+                <p>${project.description}</p>
+                <div><img class="main-image-project-page" src="${project.image[0]}"></img></div>
+                <video width="320" height="240" autoplay no sound>
+                <source src="${project.video}" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg"> </video>
+                `
+
+            } else {
+                console.log("fail")           
+            }
+        })
+    
+    }) 
+    
+
